1e1b744801f99099908baa12f6856c5758004e86
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-09-05 22:39:58 +0000

    admin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :genre
+  
+  def get_image(width, height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
