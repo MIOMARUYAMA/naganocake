d76e8dcfceeffc10c50f080c54731872ae532b9f
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-09-11 08:09:26 +0000

    public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+def top
+  @customer= Customer.find(params[:id])
+end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name)
+  end
+
 end
